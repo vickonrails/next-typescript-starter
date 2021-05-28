c7905d50221891f9f20ece66903827838320e370
vickonrails <victorofoegbu0009@gmail.com>
2021-05-28 11:00:11 +0100

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import React from "react";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
